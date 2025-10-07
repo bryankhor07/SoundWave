e5b44bbf7de17abb0ede9b86333ad0ff118d8f65
Bryan Khor <bryankhor07@gmail.com>
2025-10-07 10:26:59 -0700

    Integrated NowPlayingBar component into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import { useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { FavoritesProvider } from './contexts/FavoritesProvider'
-import { PlayerProvider } from './contexts/PlayerProvider'
-import NavBar from './components/NavBar'
-import Home from './pages/Home'
-import TrackDetails from './pages/TrackDetails'
-import ArtistDetails from './pages/ArtistDetails'
-import AlbumDetails from './pages/AlbumDetails'
-import Favorites from './pages/Favorites'
-import * as deezer from './lib/deezer.js'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { FavoritesProvider } from './contexts/FavoritesProvider';
+import { PlayerProvider } from './contexts/PlayerProvider';
+import NavBar from './components/NavBar';
+import NowPlayingBar from './components/NowPlayingBar';
+import Home from './pages/Home';
+import TrackDetails from './pages/TrackDetails';
+import ArtistDetails from './pages/ArtistDetails';
+import AlbumDetails from './pages/AlbumDetails';
+import Favorites from './pages/Favorites';
+import * as deezerAPI from './lib/deezer.js'
 
 function App() {
   // Expose Deezer API functions to window for console testing
   useEffect(() => {
-    window.deezer = deezer;
-    console.log('Deezer API functions available on window.deezer:', Object.keys(deezer));
+    window.deezer = deezerAPI;
+    console.log('Deezer API functions available on window.deezer:', Object.keys(deezerAPI));
   }, []);
 
   return (
@@ -23,13 +24,16 @@ function App() {
         <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <div className="min-h-screen bg-gradient-to-br from-purple-600 to-blue-500">
             <NavBar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/track/:id" element={<TrackDetails />} />
-              <Route path="/artist/:id" element={<ArtistDetails />} />
-              <Route path="/album/:id" element={<AlbumDetails />} />
-              <Route path="/favorites" element={<Favorites />} />
-            </Routes>
+            <div className="pb-20">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/track/:id" element={<TrackDetails />} />
+                <Route path="/artist/:id" element={<ArtistDetails />} />
+                <Route path="/album/:id" element={<AlbumDetails />} />
+                <Route path="/favorites" element={<Favorites />} />
+              </Routes>
+            </div>
+            <NowPlayingBar />
           </div>
         </Router>
       </PlayerProvider>
